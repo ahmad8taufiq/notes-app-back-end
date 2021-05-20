7fb8872cca6d7d2a259826fac00357fead629937
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2021-05-20 23:10:13 +0700

    fix update type notedId to noteId

--- src/handler.js
+++ src/handler.js
@@ -20,7 +20,7 @@ const addNoteHandler = (request, h) => {
       status: 'success',
       message: 'Catatan berhasil ditambahkan',
       data: {
-        notedId: id,
+        noteId: id,
       },
     });
     response.code(201);
